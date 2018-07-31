c3823589e5b00311590af5ccb1143bdfd4e22fc4
Lakiduck <lwalgama@hotmail.com>
2018-08-01 00:30:04 +1000

    Connected to DB, installed knex (globally)

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 var express = require('express');
 var app = express();
-
+var mysql = require('mysql');
 /*app.use('/static', express.static(path.join(__dirname, '/view')));*/
+
+var connectionmysql = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'nodeDB'
+});
+
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
   /*res.render('page.html');*/
-  res.send("Hi");
+  res.render('page');
 });
 
 app.listen(3000, function(){
   console.log('App Listening on port 3000');
+});
+
+connectionmysql.connect(function() {
+  console.log("DB Connected!");
 });
