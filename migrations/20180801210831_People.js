ea4491ab571ea2a40254bcf46eaeabe87a3ef111
Mythie <mythiedev@gmail.com>
2018-08-01 21:56:00 +1000

    Fixed knexfile.js, bookshelf.js, migration/

--- migrations/20180801210831_People.js
+++ migrations/20180801210831_People.js
@@ -3,9 +3,9 @@ exports.up = function(knex, Promise) {
   return knex.schema
     .createTable('People', function(table){
         table.increments();
-        table.string('first name').notNullable();
-        table.string('last name').notNullable();
-        table.integer('beep test score');
+        table.string('first_name').notNullable();
+        table.string('last_name').notNullable();
+        table.integer('beep_test_score');
         table.timestamps();
     });
 };
