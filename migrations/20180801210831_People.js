68a46f964e2e93727deb6710387953101e463a94
Lakiduck <lwalgama@hotmail.com>
2018-08-01 21:29:52 +1000

    updated mig file

--- migrations/20180801210831_People.js
+++ migrations/20180801210831_People.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema
+    .createTable('People', function(table){
+        table.increments();
+        table.string('first name').notNullable();
+        table.string('last name').notNullable();
+        table.integer('beep test score');
+        table.timestamps();
+    });
 };
 
 exports.down = function(knex, Promise) {
-  
+    return knex.schema
+      .dropTableIfExists('People')
 };
